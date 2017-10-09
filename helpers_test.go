235272e66c4646e2805a0e16148dc79de064946f
gsingharoy <neogauravsvnit@gmail.com>
2017-10-09 13:59:44 +0200

    Add helper functions
    Added two helper functions called fetchModelIDVal and generateModelID so that IDs for the models are automatically added.

---
+++ helpers_test.go
@@ -0,0 +1,20 @@
+package mgostore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func TestFetchModelIdVal(t *testing.T) {
+	id := bson.NewObjectId()
+	m := &mockModel{Id: id}
+	assert.Equal(t, id, fetchModelIDVal(m), "Returned value does not match the model id")
+}
+
+func TestGenerateModelId(t *testing.T) {
+	m := &mockModel{}
+	generateModelID(m)
+	assert.NotNil(t, m.Id)
+}
